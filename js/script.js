4f6b1a54c4c4b3e8eaa03b3a9d92b09ae7aac172
Qingjie Shen (Clark) <shen0126@algonquinlive.com>
2024-01-16 12:23:34 -0500

    change alert to text

--- js/script.js
+++ js/script.js
@@ -3,11 +3,25 @@ const inputNum = document.getElementById("input-number");
 const result = document.getElementById("output");
 
 const romanNumeralConverter = (number) => {
+    resultClean();
     number = parseInt(inputNum.value);
     if (isNaN(number)) {
-        alert("Please enter a valid number");
+        resultDisplay("Please enter a valid number");
     } else if ( !number || number <= 0) {
-        alert("Please enter a number greater than or equal to 1");
+        resultDisplay("Please enter a number greater than or equal to 1");
+    } else if (number > 3999) {
+        resultDisplay("Please enter a number less than or equal to 3999");
     }
 }
+
+const resultDisplay = (text) => {
+    result.classList.remove("hidden");
+    result.innerHTML = `<p>${text}</p>`
+}
+
+const resultClean = () => {
+    result.classList.add("hidden");
+    result.innerHTML = "";
+}
+
 convertBtn.addEventListener("click", romanNumeralConverter);
